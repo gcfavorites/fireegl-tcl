7882dcf4dae60559cb7a2835f6c7525a0e10544c
FireEgl <FireEgl@52514dda-a497-11dd-9b86-41075523de00>
2009-10-03 01:48:00 +0000

    Take into account (log)files that get rotated.  (untested)

--- trunk/eggdrop/tailf2channel.tcl
+++ trunk/eggdrop/tailf2channel.tcl
@@ -5,7 +5,13 @@ namespace eval tailf2channel {
 		if {![catch { open $filename r } fid]} {
 			fconfigure $fid -buffering line
 			if {[info exists Tails([set channel [string tolower $channel]],$filename)]} {
-				seek $fid $Tails($channel,$filename) start
+				if {$Tails(pos,$channel,$filename) >= [file size $filename]} {
+					# File shrunk since we last read from it..(log got rotated?)..Set position to the start of the file:
+					seek $fid 0 start
+				} else {
+					# Just read from where we left off at:
+					seek $fid $Tails($channel,$filename) start
+				}
 				while {[gets $fid line] >= 0} { if {[string length $line] > 0} { puthelp "PRIVMSG $channel :$line" } }
 			} else {
 				# This will make it skip everything currently in the file and only show the newly added lines..
