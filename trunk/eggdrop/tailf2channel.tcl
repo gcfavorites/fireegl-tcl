a09998e78f5558599763b7ba860dc7cf951bdbeb
FireEgl <FireEgl@52514dda-a497-11dd-9b86-41075523de00>
2009-10-03 02:01:14 +0000

    Updated to kill utimers before starting new ones.  (untested)

--- trunk/eggdrop/tailf2channel.tcl
+++ trunk/eggdrop/tailf2channel.tcl
@@ -1,9 +1,9 @@
 
 namespace eval tailf2channel {
 	proc tailf2channel {channel filename {seconds {9}}} {
-		variable Tails
 		if {![catch { open $filename r } fid]} {
 			fconfigure $fid -buffering line
+			variable Tails
 			if {[info exists Tails([set channel [string tolower $channel]],$filename)]} {
 				if {$Tails($channel,$filename) >= [file size $filename]} {
 					# File shrunk since we last read from it..(log got rotated?)..Set position to the start of the file:
@@ -12,7 +12,11 @@ namespace eval tailf2channel {
 					# Just read from where we left off at:
 					seek $fid $Tails($channel,$filename) start
 				}
-				while {[gets $fid line] >= 0} { if {[string length $line] > 0} { puthelp "PRIVMSG $channel :$line" } }
+				while {[gets $fid line] >= 0} { if {[string length $line] > 0} {
+					#if {[string match -nocase {*PATTERN HERE*} $line]} {
+						puthelp "PRIVMSG $channel :$line" }
+					#}
+				}
 			} else {
 				# This will make it skip everything currently in the file and only show the newly added lines..
 				seek $fid 0 end
@@ -20,8 +24,13 @@ namespace eval tailf2channel {
 			set Tails($channel,$filename) [tell $fid]
 			close $fid
 		}
-		utimer $seconds [list [namespace current]::tailf2channel $channel $filename $seconds]
+		variable Timers
+		set Timers($channel,$filename) [utimer $seconds [list [namespace current]::tailf2channel $channel $filename $seconds]]
 	}
+
+	# Kill the current timers so we can start new ones:
+	variable Timers
+	foreach t [array names Timers] { catch { killutimer $Timers($t) } }
 
 	# Change the next line for your channel and file you want to tail:
 	tailf2channel #Tcl /tmp/test.log 5
