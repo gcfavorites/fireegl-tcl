a2ec77bce1b6a587fae9fd1bc43c13c429f8920e
FireEgl <FireEgl@52514dda-a497-11dd-9b86-41075523de00>
2011-02-26 17:05:19 +0000

    Couple more fixes, and putlogs so you know what the replies are.

--- eggdrop/winident2.tcl
+++ eggdrop/winident2.tcl
@@ -6,20 +6,23 @@ namespace eval winident2 {
 	proc Control {idx line} {
 		if {$line ne {}} {
 			if {[regexp -- {^(\d+)\s*,\s*(\d+)} $line -> lport rport]} {
-				if {$lport > 0 && $lport < 65535 && $rport > 0 && $rport < 65535 && $rport == [lindex [split $::server :] end]} {
+				if {$lport > 0 && $lport < 65535 && $rport > 0 && $rport < 65535} {
 					putdcc $idx "$lport,$rport : USERID : UNIX : $::username"
+					putlog "Identd: Replied: $lport,$rport : USERID : UNIX : $::username"
 				} else {
 					putdcc $idx "$lport,$rport : ERROR : INVALID-PORT"
+					putlog "Identd: Replied: $lport,$rport : ERROR : INVALID-PORT"
 				}
 			} else {
 				putdcc $idx "[string range $line 0 12] : ERROR : UNKNOWN-ERROR"
+				putlog "Identd: Replied: [string range $line 0 12] : ERROR : UNKNOWN-ERROR"
 			}
 			killdcc $idx
 		}
-		return 0
+		return 1
 	}
-	proc Connect {idx} { control $idx Control }
-	proc Enable {event} { catch { listen 113 script ::winident2::Connect } }
+	proc Connect {idx} { control $idx ::winident2::Control }
+	proc Enable {event} { catch { listen 113 script ::winident2::Connect pub } }
 	proc Disable {event} { catch { listen 113 off } }
 	bind evnt - connect-server ::winident2::Enable
 	bind evnt - init-server ::winident2::Disable
@@ -29,4 +32,5 @@ namespace eval winident2 {
 	bind evnt - sighup ::winident2::Disable
 	bind evnt - prerehash ::winident2::Disable
 	bind evnt - prerestart ::winident2::Disable
+	putlog {winident2.tcl v2.0 by FireEgl - Loaded.}
 }
