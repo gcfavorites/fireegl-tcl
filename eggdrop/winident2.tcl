86d11c84f93412a8224873d81a2845809f21ec1a
FireEgl <FireEgl@52514dda-a497-11dd-9b86-41075523de00>
2011-02-26 20:10:46 +0000

    More tweaks.

--- eggdrop/winident2.tcl
+++ eggdrop/winident2.tcl
@@ -1,29 +1,45 @@
-# winident2.tcl v2.0 - by FireEgl@EFNet <Winident@FireEgl.Com> - February, 2011
+# winident2.tcl v2.0 - by FireEgl <Winident@FireEgl.Com> - February 26, 2011
 
 # Implementation of an ident server meant to be used on Windrop (Eggdrop) or Tcldrop running on Windows.
 
 namespace eval winident2 {
 	proc Control {idx line} {
 		if {$line ne {}} {
-			if {[regexp -- {^(\d+)\s*,\s*(\d+)} $line -> lport rport]} {
+			if {[regexp -- {^(\d+)\s*,\s*(\d+)} [set line [string range $line 0 12]] -> lport rport]} {
 				if {$lport > 0 && $lport < 65535 && $rport > 0 && $rport < 65535} {
-					putdcc $idx "$lport,$rport : USERID : UNIX : $::username"
-					putlog "Identd: Replied: $lport,$rport : USERID : UNIX : $::username"
+					putdcc $idx "$lport, $rport : USERID : UNIX : $::username"
+					putlog "Identd: Replied: $lport, $rport : USERID : UNIX : $::username"
 				} else {
-					putdcc $idx "$lport,$rport : ERROR : INVALID-PORT"
-					putlog "Identd: Replied: $lport,$rport : ERROR : INVALID-PORT"
+					putdcc $idx "$lport, $rport : ERROR : INVALID-PORT"
+					putlog "Identd: Replied: $lport, $rport : ERROR : INVALID-PORT"
 				}
 			} else {
-				putdcc $idx "[string range $line 0 12] : ERROR : UNKNOWN-ERROR"
-				putlog "Identd: Replied: [string range $line 0 12] : ERROR : UNKNOWN-ERROR"
+				putdcc $idx "$line : ERROR : UNKNOWN-ERROR"
+				putlog "Identd: Replied: $line : ERROR : UNKNOWN-ERROR"
 			}
-			killdcc $idx
 		}
 		return 1
 	}
-	proc Connect {idx} { control $idx ::winident2::Control }
-	proc Enable {event} { catch { listen 113 script ::winident2::Connect pub } }
-	proc Disable {event} { catch { listen 113 off } }
+	proc Connect {idx} {
+		control $idx ::winident2::Control
+		Disable
+	}
+	proc Enable {args} {
+		variable Enabled
+		if {!$Enabled && ![catch { listen 113 script ::winident2::Connect pub }]} {
+			variable Enabled 1
+			putloglev d - {Identd: Enabled.}
+		}
+	}
+	proc Disable {args} {
+		variable Enabled
+		if {$Enabled && ![catch { listen 113 off }]} {
+			putloglev d - {Identd: Disabled.}
+		}
+		variable Enabled 0
+	}
+	variable Enabled
+	if {![info exists Enabled]} { variable Enabled 0 }
 	bind evnt - connect-server ::winident2::Enable
 	bind evnt - init-server ::winident2::Disable
 	bind evnt - sigterm ::winident2::Disable
@@ -34,3 +50,5 @@ namespace eval winident2 {
 	bind evnt - prerestart ::winident2::Disable
 	putlog {winident2.tcl v2.0 by FireEgl - Loaded.}
 }
+
+# http://tools.ietf.org/html/rfc1413
